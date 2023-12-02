470d91837119bcfbc8ec58aa6b368d90182051ec
Your GitHub Username <k-lam02@outlook.com>
2023-12-02 17:55:43 +0000

    Create register form

--- job/jobtracker/userauth/views.py
+++ job/jobtracker/userauth/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 # Create your views here.
 def registerPage(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+
     context = {'form':form}
     return render(request, "register.html",context)
 
