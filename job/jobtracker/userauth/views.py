e9ae6e58c8d217ac5db5bc241e27ef5d6a7f0c12
Your GitHub Username <k-lam02@outlook.com>
2023-12-02 17:13:49 +0000

    initial

--- job/jobtracker/userauth/views.py
+++ job/jobtracker/userauth/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def registerPage(request):
+    form = UserCreationForm()
+    context = {'form':form}
+    return render(request, "register.html",context)
+
+def loginPage(request):
+    return render(request, "login.html")
